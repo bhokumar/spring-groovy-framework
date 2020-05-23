8cd610bca3a98cd33a72e1138f77af00787fa24c
bhokumar <bhoopendra.akgec@gmail.com>
2020-05-24 00:11:46 +0530

    Adding sample spring boot application

---
+++ sample-spring-boot-annotation/src/main/groovy/com/github/bhokumar/samplespringbootannotation/SampleSpringBootAnnotationApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.bhokumar.samplespringbootannotation
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SampleSpringBootAnnotationApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAnnotationApplication, args)
+	}
+
+}
---
+++ sample-spring-boot-annotation/src/test/groovy/com/github/bhokumar/samplespringbootannotation/SampleSpringBootAnnotationApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.bhokumar.samplespringbootannotation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleSpringBootAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
